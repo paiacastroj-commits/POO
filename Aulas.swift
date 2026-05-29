e5810eb9ab53bfe77c691a1c46cf0cf8db3868d8
paiacastroj-commits <paiacastroj@gmail.com>
2026-05-29 18:22:32 -0300

    Create Aulas.swift

---
+++ Aulas.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum CategoriaAula: String {
+    case musculacao = "Musculação"
+    case spinning = "Spinning"
+    case yoga = "Yoga"
+    case funcional = "Funcional"
+    case luta = "Luta"
+}
